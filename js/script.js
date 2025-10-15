ef25ee1b1ed0fb0c9cde1340da698e6b267b1355
Michael Rodriguez <miesroro@gmail.com>
2025-10-15 16:19:35 -0500

    Conics proyects

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector('#hero-description');
+let navlist =document.querySelector('.hero-section');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal (
+    {
+        distance: '65px',
+        duration: 2600,
+        delay: 450,
+        reset: true
+    }
+);
+
+/* Hero Animations */
+
+sr.reveal('h1', {delay:200, origin:'top', reset:false});
+sr.reveal('h2', {delay:200, origin:'top', reset:false});
+sr.reveal('p',{delay:200,origin:'left',reset:false});
+sr.reveal('.hero-img',{delay:500, origin:'right', reset:false});
+
+//Footer animations
+sr.reveal('footer',{delay:50,origin:'bottom', reset:false});
+// Show borderbox deppending to page status 
