8de412e71f1fe7779ec5925a31aba705c54c27f1
Michael Rodriguez <miesroro@gmail.com>
2025-10-17 19:12:51 -0500

    add animations in english site

--- js/script.js
+++ js/script.js
@@ -15,25 +15,12 @@ const sr = ScrollReveal (
     }
 );
 
-/* Hero Animations */
-
 sr.reveal('h1', {delay:200, origin:'top', reset:false});
 sr.reveal('h2', {delay:200, origin:'top', reset:false});
-sr.reveal('p',{delay:200,origin:'left',reset:false});
+sr.reveal('.p-description',{delay:200,origin:'left',reset:false});
 sr.reveal('.hero-img',{delay:500, origin:'right', reset:false});
+sr.reveal('.img-description',{delay:200, origin:'right', reset:false});
 
 //Footer animations
 sr.reveal('footer',{delay:50,origin:'bottom', reset:false});
 // Show borderbox deppending to page status 
-
-// Show borderbox deppending to page status
-
-function openFullscreen(img) {
-    if (img.requestFullscreen) {
-        img.requestFullscreen();
-    }else if (img.webkitRequestFullscreen) { /* Safari */
-    img.webkitRequestFullscreen();
-    } else if (img.msRequestFullscreen) { /* IE11 */
-    img.msRequestFullscreen();
-}
-}
